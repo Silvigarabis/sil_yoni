9944269d54845060522a51f0f3c114e0b6df0b77
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2023-07-22 06:51:42 +0800

    c

--- check-json.sh
+++ check-json.sh
@@ -1,6 +1,12 @@
 find -name \*.json | while read -r f; do
+{
     result=`jq . "$f" 2>&1 1>/dev/null`
     if [ "${#result}" != 0 ]; then
         echo "$f: $result"
     fi
+} &
+done
+
+for wpid in `jobs -p`; do
+	wait $wpid
 done
