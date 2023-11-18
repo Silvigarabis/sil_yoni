0285b06d120baf773d2d76b774606cd7b86e14de
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2023-11-19 04:35:55 +0800

    m

--- src/main/build.sh
+++ src/main/build.sh
@@ -2,5 +2,24 @@
 
 set -xe
 
-node scripts/cover_yaml_to_json.js data_resources resources
-( cd resources && zip -ur ../../../../../mods/sil_yoni-1.3.0.jar data/ assets/ )
+build1(){
+   node scripts/cover_yaml_to_json.js data_resources resources
+   (
+   cd resources
+   zip -r ../../../../../mods/sil_yoni-1.3.0.jar assets/ data/
+   )
+}
+build2(){
+   node scripts/cover_yaml_to_json.js data_resources resources
+   (cd ../..
+   vhomeexec sh gradlew build
+   cp build/libs/sil_yoni-1.3.0.jar ../sil_yoni-1.3.0.jar
+   )
+   cp build/libs/sil_yoni-1.3.0.jar ../../mods/sil_yoni-1.3.0.jar
+}
+
+build1
+
+
+
+cp ../../../../mods/sil_yoni-1.3.0.jar ../../../sil_yoni-1.3.0.jar
--- src/main/java/io/github/silvigarabis/sil_yoni/SilYoniMod.java
+++ src/main/java/io/github/silvigarabis/sil_yoni/SilYoniMod.java
@@ -1,41 +1,59 @@
 package io.github.silvigarabis.sil_yoni;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-
+import java.util.Map;
+import java.util.HashMap;
 import net.minecraft.util.Identifier;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
 
 public class SilYoniMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("SIL YONI");
-
-    public static final Identifier SOUND_ENTITY_FAIRY_FLY_ID = new Identifier("sil_yoni:entity.fairy.fly");
-    public static final SoundEvent SOUND_ENTITY_FAIRY_FLY_EVENT = SoundEvent.of(SOUND_ENTITY_FAIRY_FLY_ID);
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    private static final Logger LOGGER = LoggerFactory.getLogger("SIL YONI");
+    private static final Map<Identifier, SoundEvent> soundRegistries = new HashMap<>();
+    private static void addSound(String name){
+        Identifier id = new Identifier(name);
+        SoundEvent eventRegistry = SoundEvent.of(id);
+        soundRegistries.put(id, eventRegistry);
+    }
+    static {
+        addSound("sil_yoni:entity.fairy.fly");
+        addSound("sil_yoni:entity.fairy2.fly");
+    }
+    private static void printSystemEnv(){
+        var envs = System.getenv();
+        for (var name: envs.keySet()){
+            LOGGER.info(name + "=" + envs.get(name));
+        }
+    }
     
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    private void registerSoundEvents(){
+        for (Map.Entry<Identifier, SoundEvent> entry : soundRegistries.entrySet()){
+            Registry.register(
+                Registries.SOUND_EVENT,
+                entry.getKey(),
+                entry.getValue()
+            );
+        }
+        
+    }
+    
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
 
-		LOGGER.info("Initialize SilYoniMod...");
-		
-		Registry.register(Registries.SOUND_EVENT, SOUND_ENTITY_FAIRY_FLY_ID, SOUND_ENTITY_FAIRY_FLY_EVENT);
-		
-		var envs = System.getenv();
-		for (var name: envs.keySet()){
-		    LOGGER.info(name + "=" + envs.get(name));
-		}
-		
-		LOGGER.info("Done.");
-		
-	}
+        LOGGER.info("Initialize SilYoniMod...");
+        
+        registerSoundEvents();
+        
+        LOGGER.info("Done.");
+        
+    }
 }
