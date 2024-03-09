7c6d1b51d39771e8158b57dbd1492dfe25aa453d
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2024-03-09 12:38:15 +0800

    add fast buildscript for me

---
+++ build-scripts/fastwatchbuild.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+java_source_time_info=""
+datapack_source_time_info=""
+
+should_rebuild_java(){
+   local time_info=$(get_time_info $(get_java_source_dir))
+   if [[ ${time_info} != ${java_source_time_info} ]]; then
+      java_source_time_info="${time_info}"
+      return 0
+   else
+      return 1
+   fi
+}
+
+should_rebuild_datapack(){
+   local time_info=$(get_time_info $(get_datapack_source_dir))
+   if [[ ${time_info} != ${datapack_source_time_info} ]]; then
+      datapack_source_time_info="${time_info}"
+      return 0
+   else
+      return 1
+   fi
+}
+
+get_time_info(){
+   find "$1" -type f,d | xargs -0 stat -c "%n %Y"
+}
+
+rebuild_java(){
+   ( cd .. && sh gradlew build )
+   . ./afterbuild.sh || true
+}
+
+rebuild_datapack(){
+   node scripts/cover_yaml_to_json.js "$(get_datapack_source_dir)" ../build/resources/main
+   local jarFile=$(realpath "../build/libs/sil_yoni-1.3.0.jar")
+   (
+      cd ../build/resources/main
+      zip -r "${jarFile}" *
+   )
+   . ./afterbuild.sh || true
+}
+
+get_java_source_dir(){
+   echo ../src/main/
+}
+
+get_datapack_source_dir(){
+   echo ../datapack/
+}
+
+cd "$(realpath "$(dirname "$BASH_SOURCE")")"
+set -u
+
+should_rebuild_datapack
+should_rebuild_java
+if [[ $# != 0 ]]; then
+   rebuild_java
+fi
+
+while sleep 3; do
+   if should_rebuild_java; then
+      rebuild_java
+   elif should_rebuild_datapack; then
+      rebuild_datapack
+   if
+done
