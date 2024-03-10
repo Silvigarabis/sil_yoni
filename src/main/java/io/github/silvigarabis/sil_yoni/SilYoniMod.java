1b87b847eb0dcd3cdcc2d246e74ce7a1905ffc02
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2024-03-11 02:51:49 +0800

    add some mixin

--- src/main/java/io/github/silvigarabis/sil_yoni/SilYoniMod.java
+++ src/main/java/io/github/silvigarabis/sil_yoni/SilYoniMod.java
@@ -15,23 +15,24 @@ public class SilYoniMod implements ModInitializer {
     // It is considered best practice to use your mod id as the logger's name.
     // That way, it's clear which mod wrote info, warnings, and errors.
     private static final Logger LOGGER = LoggerFactory.getLogger("SIL YONI");
+
+    // new sounds to be registering
     private static final Map<Identifier, SoundEvent> soundRegistries = new HashMap<>();
+
     private static void addSound(String name){
         Identifier id = new Identifier(name);
         SoundEvent eventRegistry = SoundEvent.of(id);
         soundRegistries.put(id, eventRegistry);
     }
-    static {
-        addSound("sil_yoni:entity.fairy.fly");
-        addSound("sil_yoni:entity.fairy2.fly");
-    }
+
     private static void printSystemEnv(){
+        // debug only
         var envs = System.getenv();
         for (var name: envs.keySet()){
             LOGGER.info(name + "=" + envs.get(name));
         }
     }
-    
+
     private void registerSoundEvents(){
         for (Map.Entry<Identifier, SoundEvent> entry : soundRegistries.entrySet()){
             Registry.register(
@@ -42,18 +43,24 @@ public class SilYoniMod implements ModInitializer {
         }
         
     }
-    
+
     @Override
     public void onInitialize() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
 
-        LOGGER.info("Initialize SilYoniMod...");
+        LOGGER.info("Initialize SilYoniMod... A mod just for me... to add some origin");
         
         registerSoundEvents();
         
-        LOGGER.info("Done.");
+        LOGGER.info("Done. Have a nice day.");
         
     }
+
+    static {
+        addSound("sil_yoni:entity.fairy.fly");
+        addSound("sil_yoni:entity.spirit_of_void.fly");
+    }
+
 }
