0304f32e7297250e94c7028f93aa88f0411b071d
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2024-03-11 03:24:07 +0800

    add some test code

--- src/main/java/io/github/silvigarabis/sil_yoni/mixin/BeeRideableMixin.java
+++ src/main/java/io/github/silvigarabis/sil_yoni/mixin/BeeRideableMixin.java
@@ -1,17 +1,17 @@
 package io.github.silvigarabis.sil_yoni.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.entity.passive.BeeEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
-public class ExampleMixin {
+@Mixin(BeeEntity.class)
+public class BeeRideableMixin {
    private static final Logger LOGGER = LoggerFactory.getLogger("SIL YONI");
 
-   @Inject(at = @At("HEAD"), method = "loadWorld")
-   private void worldLoading(CallbackInfo info) {
-      LOGGER.info("loading world...");
+   @Inject(at = @At("HEAD"), method = "interactMob")
+   private void onInteractMobBee(CallbackInfo info){
+      LOGGER.info("some one interact on a bee");
    }
 }
