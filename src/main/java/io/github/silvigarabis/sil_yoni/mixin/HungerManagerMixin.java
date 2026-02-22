e079abe0151451e175c5a5e4877269067a527b16
Silvigarabis <Silvigarabis@outlook.com>
2026-02-22 18:19:02 +0800

    ConvertFoodToResourcePower 添加饥饿消耗转换

--- src/main/java/io/github/silvigarabis/sil_yoni/mixin/HungerManagerMixin.java
+++ src/main/java/io/github/silvigarabis/sil_yoni/mixin/HungerManagerMixin.java
@@ -1,12 +1,16 @@
 package io.github.silvigarabis.sil_yoni.mixin;
 
 import io.github.apace100.apoli.component.PowerHolderComponent;
-import io.github.silvigarabis.sil_yoni.power.ConvertFoodToResource;
+import io.github.silvigarabis.sil_yoni.power.ConvertFoodToResourcePower;
 import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerEntity;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
@@ -15,13 +19,33 @@ import java.util.List;
 public abstract class HungerManagerMixin {
     @Unique
     private PlayerEntity player;
+    @Unique
+    private List<ConvertFoodToResourcePower> mfps;
+    @Unique
+    private boolean acceptSaturation = true;
+    @Unique
+    private boolean acceptFood = true;
+    @Unique
+    private boolean acceptExhaustion = true;
+
+    @Inject(method = "update", at = @At("HEAD"))
+    private void cachePlayer(PlayerEntity player, CallbackInfo ci) {
+        this.player = player;
+    }
+
+    @Unique
+    private void updateData() {
+        if (player == null) return;
+        mfps = PowerHolderComponent.getPowers(player, ConvertFoodToResourcePower.class);
+        acceptSaturation= mfps.stream().anyMatch(ConvertFoodToResourcePower::shouldAcceptSaturation);
+        acceptFood = mfps.stream().anyMatch(ConvertFoodToResourcePower::shouldAcceptFood);
+        acceptExhaustion = mfps.stream().anyMatch(ConvertFoodToResourcePower::shouldAcceptExhaustion);
+    }
 
     @ModifyVariable(method = "add(IF)V", at = @At(value = "HEAD"), argsOnly = true)
     public int onAddFood(int food){
         if (player != null) {
-            List<ConvertFoodToResource> mfps = PowerHolderComponent.getPowers(player, ConvertFoodToResource.class);
-
-            boolean acceptFood = mfps.stream().anyMatch(ConvertFoodToResource::shouldAcceptFood);
+            updateData();
 
             int finalFood = food;
             mfps.forEach(p -> p.addFoodToResource(finalFood));
@@ -34,9 +58,7 @@ public abstract class HungerManagerMixin {
     @ModifyVariable(method = "add(IF)V", at = @At(value = "HEAD"), argsOnly = true)
     public float onAddSaturation(float saturation) {
         if (player != null) {
-            List<ConvertFoodToResource> mfps = PowerHolderComponent.getPowers(player, ConvertFoodToResource.class);
-
-            boolean acceptSaturation = mfps.stream().anyMatch(ConvertFoodToResource::shouldAcceptSaturation);
+            updateData();
 
             float finalSaturation = saturation;
             mfps.forEach(p -> p.addSaturationToResource(finalSaturation));
@@ -46,8 +68,56 @@ public abstract class HungerManagerMixin {
         return saturation;
     }
 
-    @Inject(method = "update", at = @At("HEAD"))
-    private void cachePlayer(PlayerEntity player, CallbackInfo ci) {
-        this.player = player;
+    @Inject(
+            method = "update",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/entity/player/HungerManager;exhaustion:F",
+                    opcode = Opcodes.PUTFIELD,
+                    shift = At.Shift.AFTER,
+                    ordinal = 0
+            )
+    )
+    private void onExhaustionTriggered(PlayerEntity player, CallbackInfo ci) {
+        updateData();
+
+        // 注入点：
+        // if (this.exhaustion > 4.0F) {
+        //    this.exhaustion -= 4.0F;
+        mfps.forEach(p -> p.addExhaustionToResource(-1));
+    }
+
+    @Redirect(
+            method = "update",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/entity/player/HungerManager;foodLevel:I",
+                    opcode = Opcodes.PUTFIELD,
+                    ordinal = 0
+            )
+    )
+    private void redirectExhaustFood(HungerManager instance, int value, PlayerEntity player) {
+        updateData();
+
+        if (acceptExhaustion){
+            instance.setFoodLevel(value);
+        }
+    }
+
+    @Redirect(
+            method = "update",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/entity/player/HungerManager;saturationLevel:F",
+                    opcode = Opcodes.PUTFIELD,
+                    ordinal = 0
+            )
+    )
+    private void redirectExhaustSaturation(HungerManager instance, float value, PlayerEntity player) {
+        updateData();
+
+        if (acceptExhaustion){
+            instance.setSaturationLevel(value);
+        }
     }
 }
